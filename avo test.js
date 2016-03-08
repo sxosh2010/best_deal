74ef751510583fe962128bc5183dcbd02570e468
coder <xolelwa@projectcodex.co>
2016-03-08 13:18:19 +0200

    steal busy

---
+++ avo test.js
@@ -0,0 +1,18 @@
+function avocado(avocadostring){
+	var places = avocadostring
+	            .split(",");
+	            
+	 var avocadolist = [];
+	 for(var i=0;i<avocado.length; i++){
+	 	var place = places[i];
+	 }           
+
+}
+
+
+
+test( "avocado list", function(assert) {
+  assert.equal("1for R3 , 2forR7, 3forR10, 5forR14.50"("avocadolist!");
+});
+
+
